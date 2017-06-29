f4a936ab0d17ce98d8344917b49a7f05fe0044a6
Aviv Eyal <Aviv Eyal@Aviv>
2017-06-29 20:49:12 +0300

    jar

---
+++ SokobanService/src/main/java/fully/qualified/MainClass.java
@@ -0,0 +1,10 @@
+package fully.qualified;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SokobanService/src/test/java/com/codebind/AppTest.java
@@ -0,0 +1,11 @@
+package com.codebind;
+
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+	public void test(){
+		
+	}
+}
